79797e613a6b1111f24113539d1bc9a8a01237e5
Gerónimo Tondato <geronimo.tondato@gmail.com>
2020-04-19 20:04:08 -0300

    commit de la rama prueba1

--- practica.py
+++ practica.py
@@ -5,6 +5,7 @@ def solution(n):
         d[l]= n % 2
         n //= 2
         l += 1
+        #aca estoy agregando comentario
     for p in range(1, l//2): # <--- ACA ESTÁ EL CAMBIO!!!
         ok = True
         for i in range(l - p):
